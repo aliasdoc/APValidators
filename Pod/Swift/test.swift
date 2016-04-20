0a6f08c783d2f9406cc554338ab5573fc5ddeaf1
Nickolay Sheika <nickolai.sheika@alterplay.com>
2016-04-20 11:53:37 +0300

    project setup for swift

---
+++ Pod/Swift/test.swift
@@ -0,0 +1,13 @@
+//
+//  test.swift
+//  APValidators
+//
+//  Created by Nickolay Sheika on 4/20/16.
+//  Copyright © 2016 Nickolay Sheika. All rights reserved.
+//
+
+import Foundation
+
+class HUI: NSObject {
+    let hui = "HUI"
+}
